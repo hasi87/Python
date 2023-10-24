1634bbc19304da9460b60af617bb840af578958b
hasi87 <62594068+hasi87@users.noreply.github.com>
2023-10-24 16:23:12 +0200

    Update SimpleGame.py
    Update cause of error in Files

--- SimpleGame.py
+++ SimpleGame.py
@@ -2,7 +2,7 @@ print('Welcome to AskPython Quiz')
 answer=input('Are you ready to play the Quiz ? (yes/no) :')
 score=0
 total_questions=3
- 
+
 if answer.lower()=='yes':
     answer=input('Question 1: What is your Favourite programming language?')
     if answer.lower()=='python':
@@ -18,6 +18,7 @@ if answer.lower()=='yes':
         print('correct')
     else:
         print('Wrong Answer :(')
+
  
     answer=input('Question 3: What is the name of your favourite website for learning Python?')
     if answer.lower()=='askpython':
